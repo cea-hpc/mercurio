d7ab9031501b98d95bf4cf85e1812333fa6d49ab
Quentin Bouget <quentin.bouget@cea.fr>
2017-10-22 10:55:45 +0000

    dist: allow mercurio to be packaged with setuptools
    Add the setup.py and setup.cfg files expected by setuptools to
    package mercurio.
    
    Change-Id: I16184606f304ae99eb8a38ff91d064f6bf594d66
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Reviewed-on: https://cws-fleury.labs.ocre.cea.fr/gerrit/2962
    Reviewed-by: Aurelien Degremont <aurelien.degremont@cea.fr>
    Reviewed-by: Henri Doreau <henri.doreau@cea.fr>
    Tested-by: Jenkins s8open_nr
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>

---
+++ setup.py
@@ -0,0 +1,49 @@
+# author: Quentin Bouget <quentin.bouget@cea.fr>
+#
+
+"""
+mercurio's setup module
+"""
+
+from setuptools import setup, find_packages
+
+import mercurio
+
+setup(
+    name='mercurio',
+    version=mercurio.__version__,
+
+    # TODO: update description
+    description='A file transfer tool',
+
+    # The project's main homepage
+    url='https://github.com/cea-hpc/mercurio',
+
+    # Author details
+    author='Quentin Bouget',
+    author_email='quentin.bouget@cea.fr',
+
+    license='LGPLv3',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        ],
+
+    keywords='file-transfer',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=['parablox'],
+
+    python_requires='~=3.4',
+
+    entry_points={
+        'console_scripts': [
+            'mercurio=mercurio.cli:main',
+            ],
+        },
+)
