5cd40ced866ad99ae76d86bd2a9bd1d12e9f7aef
Quentin Bouget <quentin.bouget@cea.fr>
2019-12-16 12:43:42 +0100

    legal: license mercurio under LGPLv3-or-later
    Change-Id: I317554540ef2d06a9ec204975a872a7f0c79b192
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>

--- tests/unit/test_blocks.py
+++ tests/unit/test_blocks.py
@@ -1,5 +1,10 @@
-# author: Quentin Bouget <quentin.bouget@cea.fr>
+# This file is part of the RobinHood Library
+# Copyright (C) 2019 Commissariat a l'energie atomique et aux energies
+# 		      alternatives
 #
+# SPDX-License-Identifer: LGPL-3.0-or-later
+#
+# author: Quentin Bouget <quentin.bouget@cea.fr>
 
 """
 Test the different block classes defined in mercurio.blocks
